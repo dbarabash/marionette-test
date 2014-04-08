272583c8687f7db0af387c3e4d94c69e76ff82ad
Dmitry <dmbarabash@gmail.com>
2014-04-08 10:42:34 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MarionetteTest::Application.config.secret_token = '44d6d0b62569b56171b85ff9f68814d2074169940507a91259fb8fbef7d5273c7a1a1c03210fb957f9e0143feaf3481b1d74c9a52718cd8af44906092b72bd4b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MarionetteTest::Application.config.session_store :cookie_store, key: '_marionette-test_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MarionetteTest::Application.config.session_store :active_record_store
